4975326f9c6efb26d76ebac6df5bfc047d2abe33
Jalen Vaughn <javaugh@iu.edu>
2024-11-11 16:22:39 -0500

    Added targeted inspections for PHP-CS-Fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,5 +6,5 @@ $finder = (new PhpCsFixer\Finder())
     ->in([__DIR__ . '/config', __DIR__ . '/public', __DIR__ . '/src', __DIR__ . '/tests']);
 
 return (new PhpCsFixer\Config())
-    ->setRules(['@PER-CS2.0' => true])
+    ->setRules(['@PER-CS2.0' => true, '@PHP82Migration' => true])
     ->setFinder($finder);
