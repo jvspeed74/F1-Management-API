43c78f6a3123511cca4376538743033d8a44740e
jvspeed74 <javaugh@iu.edu>
2024-12-01 12:00:49 -0500

    require /templates instead of /app
    Signed-off-by: jvspeed74 <javaugh@iu.edu>

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../app/header.php';
-require __DIR__ . '/../app/signin.php';
-require __DIR__ . '/../app/signup.php';
-require __DIR__ . '/../app/main.php';
-require __DIR__ . '/../app/footer.php';
-require __DIR__ . '/../app/modal.php';
+require __DIR__ . '/../templates/header.php';
+require __DIR__ . '/../templates/signin.php';
+require __DIR__ . '/../templates/signup.php';
+require __DIR__ . '/../templates/main.php';
+require __DIR__ . '/../templates/footer.php';
+require __DIR__ . '/../templates/modal.php';
