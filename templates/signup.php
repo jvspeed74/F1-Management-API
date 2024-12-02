5ad74dd6ee6e6449b53dfa28a87059bda033fea9
jvspeed74 <javaugh@iu.edu>
2024-12-01 20:51:49 -0500

    Removed email field
    Signed-off-by: jvspeed74 <javaugh@iu.edu>

--- templates/signup.php
+++ templates/signup.php
@@ -15,8 +15,6 @@
         <input type="text" id="signup-name" class="form-control" placeholder="Name" required autofocus>
         <label for="username" class="sr-only">Username</label>
         <input type="text" id="signup-username" class="form-control" placeholder="Username" required>
-        <label for="email" class="sr-only">Email</label>
-        <input type="email" id="signup-email" class="form-control" placeholder="Email" required>
         <label for="password" class="sr-only">Password</label>
         <input type="password" id="signup-password" class="form-control" placeholder="Password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
