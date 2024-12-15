d108f1c9dce0bb9ecccfbd4b2a31153c70ad44b1
jvspeed74 <javaugh@iu.edu>
2024-12-14 19:57:45 -0500

    Added AuthController.php which handles authentication requests.
    Heavily refactored JWTAuthenticator by removing revoke method and introducing separation of concerns with TokenRepository.
    
    Signed-off-by: jvspeed74 <javaugh@iu.edu>

--- src/Authentication/JWTAuthenticator.php
+++ src/Authentication/JWTAuthenticator.php
@@ -16,76 +16,59 @@ use Psr\Log\LoggerInterface;
 class JWTAuthenticator
 {
     private string $secretKey;
-    private TokenRepository $tokenRepository;
     private LoggerInterface $logger;
 
     public function __construct(TokenRepository $tokenRepository, LoggerInterface $logger)
     {
         $this->secretKey = getenv('JWT_SECRET_KEY') ?: 'secret';
-        $this->tokenRepository = $tokenRepository;
         $this->logger = $logger;
     }
 
     /**
-     * @param string $token
-     * @return array<mixed>
+     * Validate a JWT token, receiving the claims if valid or an error message if not.
+     *
+     * @param string $token The JWT token to validate.
+     * @return array<mixed> The claims if the token is valid, or an error message if not.
      */
     public function validate(string $token): array
     {
         try {
+            $this->logger->debug('Validating token:', ['token' => $token]);
             $decoded = JWT::decode($token, new Key($this->secretKey, 'HS256'));
-            return (array) $decoded;
         } catch (ExpiredException $e) {
-            $this->logger->warning('Token expired', ['exception' => $e]);
+            $this->logger->debug('Token expired', ['exception' => $e]);
             return ['error' => 'Token expired'];
         } catch (SignatureInvalidException $e) {
-            $this->logger->warning('Invalid token signature', ['exception' => $e]);
+            $this->logger->debug('Invalid token signature', ['exception' => $e]);
             return ['error' => 'Invalid token signature'];
         } catch (BeforeValidException $e) {
-            $this->logger->warning('Token not valid yet', ['exception' => $e]);
+            $this->logger->debug('Token not valid yet', ['exception' => $e]);
             return ['error' => 'Token not valid yet'];
         } catch (\Exception $e) {
-            $this->logger->error('Invalid token', ['exception' => $e]);
+            $this->logger->error('Caught an unexpected exception while validating', ['exception' => $e]);
             return ['error' => 'Invalid token'];
         }
+        return (array) $decoded;
     }
 
     /**
      * @param array<mixed> $claims
-     * @return string
+     * @param int $secondsUntilExpiration
+     * @return array{token: string, exp: int}
      */
-    public function generate(array $claims = []): string
+    public function generate(array $claims = [], int $secondsUntilExpiration = 3600): array
     {
         $issuedAt = time();
-        $expirationTime = $issuedAt + 3600; // jwt valid for 1 hour
+        $expiresAt = $issuedAt + $secondsUntilExpiration;
         $payload = array_merge($claims, [
             'iat' => $issuedAt,
-            'exp' => $expirationTime,
+            'exp' => $expiresAt,
         ]);
 
-        $jwtToken = JWT::encode($payload, $this->secretKey, 'HS256');
-        $this->tokenRepository->create([
-            'token' => $jwtToken,
-            'token_type' => 'jwt',
-            'expires_at' => date('Y-m-d H:i:s', $expirationTime),
-        ]);
-
-        return $jwtToken;
-    }
-
-    public function revoke(string $token): void
-    {
-        /** @var Token|null $tokenModel */
-        $tokenModel = $this->tokenRepository->findBy('token', $token);
-        if ($tokenModel === null) {
-            return;
-        }
-
-        // Implement token revocation logic, e.g., adding to a blacklist
-        if ($this->tokenRepository->delete($tokenModel->id)) {
-            $this->logger->info('Token revoked', ['token' => $token]);
-        } else {
-            $this->logger->error('Failed to revoke token', ['token' => $token]);
-        }
+        $token = JWT::encode($payload, $this->secretKey, 'HS256');
+        return [
+            'token' => $token,
+            'exp' => $expiresAt
+        ];
     }
 }
---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,182 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Authentication\JWTAuthenticator;
+use App\Models\User;
+use App\Repositories\TokenRepository;
+use App\Repositories\UserRepository;
+use DateTime;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpInternalServerErrorException;
+
+class AuthController
+{
+    private UserRepository $userRepository;
+    private JWTAuthenticator $jwtAuthenticator;
+    private TokenRepository $tokenRepository;
+    private LoggerInterface $logger;
+
+    public function __construct(
+        UserRepository $userRepository,
+        JWTAuthenticator $jwtAuthenticator,
+        TokenRepository $tokenRepository,
+        LoggerInterface $logger,
+    ) {
+        $this->userRepository = $userRepository;
+        $this->jwtAuthenticator = $jwtAuthenticator;
+        $this->tokenRepository = $tokenRepository;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function login(Request $request, Response $response): Response
+    {
+        $this->logger->info('Handling login request');
+        $data = json_decode(
+            (string)$request->getBody(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR,
+        );
+        $username = $data['username'] ?? null;
+        if (!$username) {
+            throw new HttpBadRequestException($request, 'Username is required');
+        }
+
+        /** @var User|null $user */
+        $user = $this->userRepository->findBy('username', $username);
+        if (!$user || !password_verify($data['password'], $user->password)) {
+            throw new HttpBadRequestException($request, 'Invalid credentials');
+        }
+
+        $this->logger->info('User authenticated. Generating token');
+        $tokenData = $this->jwtAuthenticator->generate(
+            ['user_id' => $user->id, 'username' => $user->username],
+        );
+        $this->tokenRepository->create([
+            'user_id' => $user->id,
+            'token' => $tokenData['token'],
+            'token_type' => 'jwt',
+            'revoked' => false,
+            'expires_at' => (new DateTime())->setTimestamp($tokenData['exp']),
+        ]);
+
+        $response->getBody()->write(
+            json_encode(['token' => $tokenData['token']], JSON_THROW_ON_ERROR),
+        );
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function register(Request $request, Response $response): Response
+    {
+        $this->logger->info('Handling register request');
+        $data = json_decode(
+            (string)$request->getBody(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR,
+        );
+        $hashedPassword = password_hash($data['password'], PASSWORD_DEFAULT);
+        if (!$hashedPassword) {
+            throw new HttpInternalServerErrorException(
+                $request,
+                'Failed to hash password',
+            );
+        }
+
+        if ($this->userRepository->findBy('username', $data['username'])) {
+            $this->logger->warning(
+                'Username already exists',
+                ['username' => $data['username']],
+            );
+            $response->getBody()->write(
+                json_encode(['error' => 'Username already exists'],
+                    JSON_THROW_ON_ERROR),
+            );
+            return $response->withStatus(400)->withHeader(
+                'Content-Type',
+                'application/json',
+            );
+        }
+
+        /** @var User $user */
+        $user = $this->userRepository->create([
+            'username' => $data['username'],
+            'password' => $hashedPassword,
+        ]);
+
+        $this->logger->info(
+            'User registered successfully',
+            ['username' => $user->username],
+        );
+        $response->getBody()->write(
+            json_encode(['message' => 'User registered successfully'],
+                JSON_THROW_ON_ERROR),
+        );
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(
+            201,
+        );
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    public function revoke(Request $request, Response $response): Response
+    {
+        $this->logger->info('Handling revoke request');
+        $authHeader = $request->getHeader('Authorization')[0] ?? null;
+        if ($authHeader === null) {
+            $this->logger->warning('Authorization header missing');
+            $response->getBody()->write(
+                json_encode(['error' => 'Authorization header missing'],
+                    JSON_THROW_ON_ERROR),
+            );
+            return $response->withStatus(400)->withHeader(
+                'Content-Type',
+                'application/json',
+            );
+        }
+
+        if (!preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
+            $this->logger->warning('Invalid Authorization header format');
+            $response->getBody()->write(
+                json_encode(['error' => 'Invalid Authorization header format'],
+                    JSON_THROW_ON_ERROR),
+            );
+            return $response->withStatus(400)->withHeader(
+                'Content-Type',
+                'application/json',
+            );
+        }
+
+        $token = $matches[1];
+        if ($this->tokenRepository->revoke($token) == 0) {
+            $this->logger->error('Failed to revoke token', ['token' => $token]);
+            $response->getBody()->write(
+                json_encode(['error' => 'Failed to revoke token'],
+                    JSON_THROW_ON_ERROR),
+            );
+            return $response->withStatus(500)->withHeader(
+                'Content-Type',
+                'application/json',
+            );
+        }
+
+        $this->logger->info('Revoke request successful');
+        $response->getBody()->write(
+            json_encode(['message' => 'Token revokation successful'], JSON_THROW_ON_ERROR),
+        );
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
